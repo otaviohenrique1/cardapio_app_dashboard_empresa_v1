17da59a917b193b7adec796085a8e71c7b81d863
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-12 20:03:27 -0300

    remonevndo codigo comentado adicionando rotas administrador

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,4 +1,3 @@
-// import { ReactNode, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { Login } from "./Login";
@@ -6,18 +5,20 @@ import { Pagina404 } from "./Pagina404";
 import { EmpresaCadastro } from "./Empresa/EmpresaCadastro";
 import { EmpresaDados } from "./Empresa/EmpresaDados";
 import { EmpresaEdicao } from "./Empresa/EmpresaEdicao";
-import { UsuarioCadastro } from "./Usuario/UsuarioCadastro";
-import { UsuarioDados } from "./Usuario/UsuarioDados";
-import { UsuarioEdicao } from "./Usuario/UsuarioEdicao";
+import { AdministradorCadastro } from "./Administrador/AdministradorCadastro";
+import { AdministradorDados } from "./Administrador/AdministradorDados";
+import { AdministradorEdicao } from "./Administrador/AdministradorEdicao";
+import { ListaAdministradores } from "./Administrador/ListaAdministradores";
 
 export function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
-        <Route path="/usuario/cadastro" element={<UsuarioCadastro/>} />
-        <Route path="/usuario/:id" element={<UsuarioDados/>} />
-        <Route path="/usuario/:id/edicao" element={<UsuarioEdicao />} />
+        <Route path="/administrador/lista" element={<ListaAdministradores/>} />
+        <Route path="/administrador/cadastro" element={<AdministradorCadastro/>} />
+        <Route path="/administrador/:id" element={<AdministradorDados/>} />
+        <Route path="/administrador/:id/edicao" element={<AdministradorEdicao />} />
         <Route path="/home" element={<HomePage/>} />
         <Route path="/empresa/cadastro" element={<EmpresaCadastro/>} />
         <Route path="/empresa/:id" element={<EmpresaDados />} />
@@ -27,15 +28,3 @@ export function RoutesApp() {
     </BrowserRouter>
   );
 }
-
-// interface AuthProviderProps {
-//   children: ReactNode;
-// }
-
-// export function AuthProvider(props: AuthProviderProps) {
-//   let [user, setUser] = useState<any>(null);
-
-//   return (
-//     <div></div>
-//   );
-// }

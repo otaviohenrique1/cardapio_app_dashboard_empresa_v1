eae66047ef70afa567aedb8f77c9b51b7a3aa839
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-12 20:25:40 -0300

    adicionando:   constantes de texto e numero   valoresIniciaisFormularioAdministrador   valoresIniciaisEmpresaDados   valoresIniciaisAdministradorDados alterando:   validacaoSchemaFormularioEmpresa   validacaoSchemaFormularioAdministrador   schemaValidacaoFormularioLogin

--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -1,15 +1,31 @@
 import * as Yup from "yup";
+import { Mensagem } from "./utils";
 
-export const valoresIniciaisFormularioUsuario: FormularioUsuarioTypes = {
+export const EMAIL_INVALIDO = "E-mail invalido";
+export const MINIMO_CARACTERES = "Minimo 8 carateres";
+export const MAXIMO_CARACTERES = 'Maximo 64 carateres';
+export const valor_minimo_carateres = 8;
+export const valor_maximo_carateres = 64;
+
+export const valoresIniciaisFormularioAdministrador: FormularioAdministradorTypes = {
   nome: "",
   email: "",
   senha: "",
 };
 
-export const validacaoSchemaFormularioUsuario = Yup.object().shape({
-  nome: Yup.string().required("Campo vazio"),
-  email: Yup.string().email("E-mail invalido").required("Campo vazio"),
-  senha: Yup.string().min(8, "Minimo 8 carateres").max(64, 'Maximo 64 carateres').required("Campo vazio"),
+export const validacaoSchemaFormularioAdministrador = Yup.object().shape({
+  nome: Yup
+    .string()
+    .required(Mensagem.GeraMensagemErro('nome')),
+  email: Yup
+    .string()
+    .email(EMAIL_INVALIDO)
+    .required(Mensagem.GeraMensagemErro('email')),
+  senha: Yup
+    .string()
+    .min(valor_minimo_carateres, MINIMO_CARACTERES)
+    .max(valor_maximo_carateres, MAXIMO_CARACTERES)
+    .required(Mensagem.GeraMensagemErro('senha')),
 });
 
 export const dadosIniciaisFormularioLogin: FormularioLoginTypes = {
@@ -18,12 +34,19 @@ export const dadosIniciaisFormularioLogin: FormularioLoginTypes = {
 };
 
 export const schemaValidacaoFormularioLogin = Yup.object().shape({
-  email: Yup.string().required('Campo vazio'),
-  senha: Yup.string().required('Campo vazio'),
+  email: Yup
+    .string()
+    .email(EMAIL_INVALIDO)
+    .required(Mensagem.GeraMensagemErro('email')),
+  senha: Yup
+    .string()
+    .min(valor_minimo_carateres, MINIMO_CARACTERES)
+    .max(valor_maximo_carateres, MAXIMO_CARACTERES)
+    .required(Mensagem.GeraMensagemErro('senha')),
 });
 
 /* Alterar se precisar colocar atributo cadastro ativo */
-export const valoresIniciaisFormularioEmpresa: FormularioEmpresaTypes = {
+export const valoresIniciaisFormularioEmpresa: FormularioAdministradorTypes = {
   nome: "",
   email: "",
   senha: "",
@@ -31,7 +54,38 @@ export const valoresIniciaisFormularioEmpresa: FormularioEmpresaTypes = {
 
 /* Alterar se precisar colocar atributo cadastro ativo */
 export const validacaoSchemaFormularioEmpresa = Yup.object().shape({
-  nome: Yup.string().required("Campo vazio"),
-  email: Yup.string().email("E-mail invalido").required("Campo vazio"),
-  senha: Yup.string().min(8, "Minimo 8 carateres").max(64, 'Maximo 64 carateres').required("Campo vazio"),
+  nome: Yup
+    .string()
+    .required(Mensagem.GeraMensagemErro('nome')),
+  email: Yup
+    .string()
+    .email(EMAIL_INVALIDO)
+    .required(Mensagem.GeraMensagemErro('email')),
+  senha: Yup
+    .string()
+    .min(valor_minimo_carateres, MINIMO_CARACTERES)
+    .max(valor_maximo_carateres, MAXIMO_CARACTERES)
+    .required(Mensagem.GeraMensagemErro('senha')),
 });
+
+
+export const valoresIniciaisEmpresaDados: EmpresaDadosTypes = {
+  id: "",
+  nome: "",
+  email: "",
+  senha: "",
+  // ativo: false,
+  codigo: "",
+  data_cadastro: "",
+  data_modificacao_cadastro: ""
+};
+
+export const valoresIniciaisAdministradorDados: AdministradorDadosTypes = {
+  id: "",
+  nome: "",
+  email: "",
+  senha: "",
+  codigo: "",
+  data_cadastro: "",
+  data_modificacao_cadastro: ""
+};

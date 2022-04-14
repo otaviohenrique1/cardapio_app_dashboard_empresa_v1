8fa6a87f80e70af65529ace777aea72716d3f86f
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:22:47 -0300

    reformulando arquivo

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,45 +1,76 @@
+/* Types */
+type ButtonColors =
+  "primary" |
+  "secondary" |
+  "success" |
+  "danger" |
+  "warning" |
+  "info" |
+  "light" |
+  "dark" |
+  "link";
+
+type DataHoraFormatos =
+  "HH:mm" |
+  "HH:mm:ss" |
+  "dd/MM/yyyy" |
+  "yyyy-MM-dd" |
+  'yyyy-MM-dd HH:mm' |
+  'dd/MM/yyyy HH:mm' |
+  'yyyy-MM-dd HH:mm:ss' |
+  'dd/MM/yyyy HH:mm:ss';
+
+type CampoInputTypes =
+  "text" |
+  "number" |
+  "email" |
+  "password";
+
+/* Parte da Tabela da Homepage */
 interface TabelaTypes {
+  /* Alterar se precisar colocar atributo cadastro ativo */
   id: number;
   nome: string;
   // ativo: string;
 }
 
-interface EmpresaDadosTypes {
+/* Parte do Login */
+interface LoginTypes {
+  email: string;
+  senha: string;
+}
+
+interface UsuarioLogadoTypes {
   id: string;
+  nome: string;
+}
+
+/* Parte da Empresa */
+interface EmpresaTypes {
   nome: string;
   email: string;
   senha: string;
+}
+
+interface EmpresaDadosTypes extends EmpresaTypes {
+  /* Alterar se precisar colocar atributo cadastro ativo */
+  id: string;
   // ativo: boolean;
   codigo: string;
   data_cadastro: string;
   data_modificacao_cadastro: string;
 }
 
-interface AdministradorDadosTypes {
-  id: string;
+/* Parte do Administrador */
+interface AdministradorTypes {
   nome: string;
   email: string;
   senha: string;
+}
+
+interface AdministradorDadosTypes extends AdministradorTypes {
+  id: string;
   codigo: string;
   data_cadastro: string;
   data_modificacao_cadastro: string;
-}
-
-interface FormularioLoginTypes {
-  email: string;
-  senha: string;
-}
-
-interface FormularioEmpresaTypes {
-  nome: string;
-  email: string;
-  senha: string;
-}
-
-/* Alterar se precisar colocar atributo cadastro ativo */
-interface FormularioAdministradorTypes {
-  nome: string;
-  email: string;
-  senha: string;
-  // cadastro_ativo: boolean;
 }

8b999bb4f59a1fcbf867fed7210cd876d617ac44
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:23:03 -0300

    reformulando arquivo

--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -1,74 +1,37 @@
-import * as Yup from "yup";
-import { Mensagem } from "./utils";
-
-export const EMAIL_INVALIDO = "E-mail invalido";
-export const MINIMO_CARACTERES = "Minimo 8 carateres";
-export const MAXIMO_CARACTERES = 'Maximo 64 carateres';
+export const EMAIL_INVALIDO = "Email invalido";
 export const valor_minimo_carateres = 8;
+export const MINIMO_CARACTERES = `Minimo ${valor_minimo_carateres} carateres`;
 export const valor_maximo_carateres = 64;
+export const MAXIMO_CARACTERES = `Maximo ${valor_maximo_carateres} carateres`;
 
-export const valoresIniciaisFormularioAdministrador: FormularioAdministradorTypes = {
+export const valoresIniciaisFormularioAdministrador: AdministradorTypes = {
   nome: "",
   email: "",
   senha: "",
 };
 
-export const validacaoSchemaFormularioAdministrador = Yup.object().shape({
-  nome: Yup
-    .string()
-    .required(Mensagem.GeraMensagemErro('nome')),
-  email: Yup
-    .string()
-    .email(EMAIL_INVALIDO)
-    .required(Mensagem.GeraMensagemErro('email')),
-  senha: Yup
-    .string()
-    .min(valor_minimo_carateres, MINIMO_CARACTERES)
-    .max(valor_maximo_carateres, MAXIMO_CARACTERES)
-    .required(Mensagem.GeraMensagemErro('senha')),
-});
-
-export const dadosIniciaisFormularioLogin: FormularioLoginTypes = {
+export const dadosIniciaisFormularioLogin: LoginTypes = {
   email: "",
   senha: ""
 };
 
-export const schemaValidacaoFormularioLogin = Yup.object().shape({
-  email: Yup
-    .string()
-    .email(EMAIL_INVALIDO)
-    .required(Mensagem.GeraMensagemErro('email')),
-  senha: Yup
-    .string()
-    .min(valor_minimo_carateres, MINIMO_CARACTERES)
-    .max(valor_maximo_carateres, MAXIMO_CARACTERES)
-    .required(Mensagem.GeraMensagemErro('senha')),
-});
-
-/* Alterar se precisar colocar atributo cadastro ativo */
-export const valoresIniciaisFormularioEmpresa: FormularioAdministradorTypes = {
+export const valoresIniciaisFormularioEmpresa: AdministradorTypes = {
   nome: "",
   email: "",
   senha: "",
 };
 
+export const valoresIniciaisAdministradorDados: AdministradorDadosTypes = {
+  id: "",
+  nome: "",
+  email: "",
+  senha: "",
+  codigo: "",
+  data_cadastro: "",
+  data_modificacao_cadastro: ""
+};
+
 /* Alterar se precisar colocar atributo cadastro ativo */
-export const validacaoSchemaFormularioEmpresa = Yup.object().shape({
-  nome: Yup
-    .string()
-    .required(Mensagem.GeraMensagemErro('nome')),
-  email: Yup
-    .string()
-    .email(EMAIL_INVALIDO)
-    .required(Mensagem.GeraMensagemErro('email')),
-  senha: Yup
-    .string()
-    .min(valor_minimo_carateres, MINIMO_CARACTERES)
-    .max(valor_maximo_carateres, MAXIMO_CARACTERES)
-    .required(Mensagem.GeraMensagemErro('senha')),
-});
-
-
 export const valoresIniciaisEmpresaDados: EmpresaDadosTypes = {
   id: "",
   nome: "",
@@ -80,12 +43,7 @@ export const valoresIniciaisEmpresaDados: EmpresaDadosTypes = {
   data_modificacao_cadastro: ""
 };
 
-export const valoresIniciaisAdministradorDados: AdministradorDadosTypes = {
-  id: "",
-  nome: "",
-  email: "",
-  senha: "",
-  codigo: "",
-  data_cadastro: "",
-  data_modificacao_cadastro: ""
+export const dadosIniciaisUsuarioLogado: UsuarioLogadoTypes = {
+  id: '',
+  nome: ''
 };

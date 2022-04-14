a505d3596653e6cc6e94fbdfac9eebcd926274ba
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:22:10 -0300

    organizando imports organizando codigo do useTable

--- src/pages/Administrador/ListaAdministradores/index.tsx
+++ src/pages/Administrador/ListaAdministradores/index.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useMemo, useState } from "react";
 import { useGlobalFilter, usePagination, useSortBy, useTable } from "react-table";
 import { Col, DropdownItem, DropdownMenu, DropdownToggle, Row, UncontrolledButtonDropdown } from "reactstrap";
-import { Titulo } from "../../../components/Titulo";
-import { BsFillGearFill } from "react-icons/bs";
 import styled from "styled-components";
+import { BsFillGearFill } from "react-icons/bs";
+import { Titulo } from "../../../components/Titulo";
 import { CampoFiltroGlobalTabela } from "../../../components/Filtros";
 import { ContainerApp } from "../../../components/ContainerApp";
-import api from "../../../utils/api";
 import { ExibePaginaInicioFim } from "../../../components/Paginacao/ExibePaginaInicioFim";
 import { IrParaPagina } from "../../../components/Paginacao/IrParaPagina";
 import { Paginacao } from "../../../components/Paginacao/Paginacao";
 import { QuantidadeItemsPorPagina } from "../../../components/Paginacao/QuantidadeItemsPorPagina";
-import { BotaoLink } from "../../../components/Botoes";
+import { BotaoLink } from "../../../components/Botoes/BotaoLink";
 import { Tabela } from "../../../components/Tabela";
 import { ModalErroDadosNaoCarregados } from "../../../components/Modals";
+import api from "../../../utils/api";
 
 export function ListaAdministradores() {
   const [data, setData] = useState<TabelaTypes[]>([]);
@@ -33,41 +33,55 @@ export function ListaAdministradores() {
     state, setGlobalFilter, canPreviousPage, canNextPage, pageOptions,
     pageCount, gotoPage, nextPage, previousPage, setPageSize,
     state: { pageIndex, pageSize }, } = useTable({
-      columns: useMemo(() => [{
-        Header: () => null,
-        isVisible: false,
-        id: 'usuarios',
-        hideHeader: false,
-        columns: [
-          { Header: 'id', accessor: 'id', id: 'id' },
-          { Header: 'Nome', accessor: 'nome', id: 'nome' },
-          {
-            Header: () => null,
-            id: 'menu_item',
-            Cell: (cell) => {
-              const id_empresa = cell.row.values['id'];
+      columns: useMemo(() => [
+        {
+          Header: () => null,
+          isVisible: false,
+          id: 'usuarios',
+          hideHeader: false,
+          columns: [
+            {
+              Header: 'id',
+              accessor: 'id',
+              id: 'id'
+            },
+            {
+              Header: 'Nome',
+              accessor: 'nome',
+              id: 'nome'
+            },
+            {
+              Header: () => null,
+              id: 'menu_item',
+              Cell: (cell) => {
+                const id_empresa = cell.row.values['id'];
 
-              return (
-                <UncontrolledButtonDropdownEstilizado>
-                  <DropdownToggle caret className="caret-off d-flex justify-content-center align-items-center w-50 btn-success">
-                    <BsFillGearFill size={20} />
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>
-                      <BotaoLink
-                        to={`/empresa/${id_empresa}`}
-                        color="light"
-                        className="nav-link text-center"
-                      >Exibir</BotaoLink>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledButtonDropdownEstilizado>
-              );
-            }
-          },
-        ],
-      },], [])
-      , data, initialState: { pageIndex: 0 },
+                return (
+                  <UncontrolledButtonDropdownEstilizado>
+                    <DropdownToggle caret className="caret-off d-flex justify-content-center align-items-center w-50 btn-success">
+                      <BsFillGearFill size={20} />
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>
+                        <BotaoLink
+                          to={`/empresa/${id_empresa}`}
+                          color="light"
+                          className="nav-link text-center"
+                        >Exibir</BotaoLink>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledButtonDropdownEstilizado>
+                );
+              }
+            },
+          ],
+        },
+      ], []
+      ),
+      data,
+      initialState: {
+        pageIndex: 0
+      },
     }, useGlobalFilter, useSortBy, usePagination);
 
   return (

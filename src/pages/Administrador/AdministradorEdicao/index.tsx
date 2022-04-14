617ff9758f6d3a3e6c1748b88ce808dbd1a44eed
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:20:32 -0300

    organizando imports organizando codigo do handleSubmit e do useState

--- src/pages/Administrador/AdministradorEdicao/index.tsx
+++ src/pages/Administrador/AdministradorEdicao/index.tsx
@@ -1,16 +1,18 @@
-import { Col, Row } from "reactstrap";
-import { Titulo } from "../../../components/Titulo";
-import api from "../../../utils/api";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Col, Row } from "reactstrap";
+import { Titulo } from "../../../components/Titulo";
 import { ContainerApp } from "../../../components/ContainerApp";
 import { FormularioAdministrador } from "../../../components/Formularios/FormularioAdministrador";
-import { FormatadorCrypto, FormatadorDados } from "../../../utils/utils";
-import { validacaoSchemaFormularioAdministrador, valoresIniciaisFormularioAdministrador } from "../../../utils/constantes";
 import { ModalSucessoCadastro, ModalErroCadastro } from "../../../components/Modals";
+import { valoresIniciaisFormularioAdministrador } from "../../../utils/constantes";
+import api from "../../../utils/api";
+import { validacaoSchemaFormularioAdministrador } from "../../../utils/ValidacaoSchemas";
+import { FormatadorCrypto } from "../../../utils/FormatadorCrypto";
+import { FormatadorDados } from "../../../utils/FormatadorDados";
 
 export function AdministradorEdicao() {
-  const [data, setData] = useState<FormularioAdministradorTypes>(valoresIniciaisFormularioAdministrador);
+  const [data, setData] = useState<AdministradorTypes>(valoresIniciaisFormularioAdministrador);
   const navigation = useNavigate();
 
   let { id } = useParams();
@@ -29,53 +31,42 @@ export function AdministradorEdicao() {
       });
   }, [id]);
 
-  const dadosDoUsuario: FormularioAdministradorTypes = {
+  const dadosDoUsuario: AdministradorTypes = {
     nome: data.nome || "",
     email: data.email || "",
     senha: data.senha || "",
   };
 
-  async function handleSubmit(values: FormularioAdministradorTypes) {
-    let nome = values.nome;
-    let email = values.email;
-    let senha = FormatadorCrypto.SenhaSHA512(values.senha);
-    let data_modificacao_cadastro = FormatadorDados.GeraDataFormata();
+  async function handleSubmit(values: AdministradorTypes) {
+    let { nome, email, senha } = values;
+
+    let senha_formatada = FormatadorCrypto.mensagemSHA512(senha);
+    let data_modificacao_cadastro_formatada = FormatadorDados.GeradorDataHoraFormatada("yyyy-MM-dd HH:mm:ss");
 
     const data = {
       'id': id,
       'nome': nome,
       'email': email,
-      'senha': senha,
-      'data_modificacao_cadastro': data_modificacao_cadastro,
+      'senha': senha_formatada,
+      'data_modificacao_cadastro': data_modificacao_cadastro_formatada,
     };
 
-    await api.put(`administrador/${id}`, data).then(() => {
-      ModalSucessoCadastro();
-      navigation(`/administrador/${id}`);
-    }).catch((error) => {
-      ModalErroCadastro();
-      console.error(error);
-    });
+    await api.put(`administrador/${id}`, data)
+      .then(() => {
+        ModalSucessoCadastro();
+        navigation(`/administrador/${id}`);
+      })
+      .catch((error) => {
+        ModalErroCadastro();
+        console.error(error);
+      });
   }
 
-  const texto = '0123456789';
-  let encrypt_message_texto = FormatadorCrypto.encrypt_message(texto);
-  let decrypt_message_texto = FormatadorCrypto.decrypt_message(encrypt_message_texto);
-  let decrypt_message_texto2 = FormatadorCrypto.decrypt_message("U2FsdGVkX1+MXSCJVaI5raoj2bPBf1A5hsLvWHV7gUQ=");
-  
   return (
     <ContainerApp>
       <Row>
         <Col md={12}>
           <Titulo tag="h1" className="w-100 text-center mb-5">Edição de dados</Titulo>
-        </Col>
-        <Col md={12}>
-          {/* <h6>{data.senha}</h6> */}
-          {/* <h6>{FormatadorDados.SenhaSHA512(data.senha)}</h6> */}
-          <h6>{texto}</h6>
-          <h6>{encrypt_message_texto}</h6>
-          <h6>{decrypt_message_texto}</h6>
-          <h6>{decrypt_message_texto2}</h6>
         </Col>
         <FormularioAdministrador
           initialValues={dadosDoUsuario}

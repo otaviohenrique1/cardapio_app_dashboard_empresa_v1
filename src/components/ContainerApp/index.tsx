09ebf901b5edd5b82933e6ceae9981df5a65497b
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:14:29 -0300

    organizando imports

--- src/components/ContainerApp/index.tsx
+++ src/components/ContainerApp/index.tsx
@@ -1,13 +1,14 @@
 import { ReactNode, useEffect, useState } from "react";
 import { Container } from "reactstrap";
-import { dadosIniciaisUsuarioLogado, HeaderApp, UsuarioLogadoDataTypes } from "../HeaderApp";
+import { HeaderApp } from "../HeaderApp";
+import { dadosIniciaisUsuarioLogado } from "../../utils/constantes";
 
 interface ContainerAppProps {
   children: ReactNode;
 }
 
 export function ContainerApp(props: ContainerAppProps) {
-  const [data, setData] = useState<UsuarioLogadoDataTypes>(dadosIniciaisUsuarioLogado);
+  const [data, setData] = useState<UsuarioLogadoTypes>(dadosIniciaisUsuarioLogado);
 
   useEffect(() => {
     let id = sessionStorage.getItem('id');

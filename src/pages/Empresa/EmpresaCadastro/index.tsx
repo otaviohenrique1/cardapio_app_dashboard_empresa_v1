d2341359a412f89e5237025835e2002f9be16001
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:18:47 -0300

    organizando imports organizando codigo do handleSubmit

--- src/pages/Empresa/EmpresaCadastro/index.tsx
+++ src/pages/Empresa/EmpresaCadastro/index.tsx
@@ -3,25 +3,30 @@ import { Col, Row } from "reactstrap";
 import { Titulo } from "../../../components/Titulo";
 import { ContainerApp } from "../../../components/ContainerApp";
 import { FormularioEmpresa } from "../../../components/Formularios/FormularioEmpresa";
-import { valoresIniciaisFormularioEmpresa, validacaoSchemaFormularioEmpresa } from "../../../utils/constantes";
-import { FormatadorDados } from "../../../utils/utils";
-import api from "../../../utils/api";
+import { valoresIniciaisFormularioEmpresa } from "../../../utils/constantes";
 import { ModalSucessoCadastro, ModalErroCadastro } from "../../../components/Modals";
+import api from "../../../utils/api";
+import { FormatadorCrypto } from "../../../utils/FormatadorCrypto";
+import { FormatadorDados } from "../../../utils/FormatadorDados";
+import { validacaoSchemaFormularioEmpresa } from "../../../utils/ValidacaoSchemas";
 
 export function EmpresaCadastro() {
-  async function handleSubmit(values: FormularioEmpresaTypes, helpers: FormikHelpers<FormularioEmpresaTypes>) {
+  async function handleSubmit(values: EmpresaTypes, helpers: FormikHelpers<EmpresaTypes>) {
     /* Alterar caso for colocar o atributo ativo */
+    const { nome, email, senha } = values;
 
-    let nome = values.nome;
-    let email = values.email;
-    let senha = values.senha;
+    let senha_formatada = FormatadorCrypto.mensagemSHA512(senha);
+    let data_hora_formatada = FormatadorDados.GeradorDataHoraFormatada("yyyy-MM-dd HH:mm:ss");
     // let ativo = String(values.ativo);
-    let data_cadastro = FormatadorDados.GeraDataFormata();
-    let data_modificacao_cadastro = FormatadorDados.GeraDataFormata();
 
-    const data = { nome, email, senha, /* ativo, */ data_cadastro, data_modificacao_cadastro };
-
-    // console.log(data);
+    const data = {
+      nome,
+      email,
+      senha: senha_formatada,
+      /* ativo, */
+      data_cadastro: data_hora_formatada,
+      data_modificacao_cadastro: data_hora_formatada
+    };
 
     await api.post('usuario', data)
       .then(() => {
